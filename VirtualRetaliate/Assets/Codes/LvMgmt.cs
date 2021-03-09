dce935bacb4d851bd4462306afdc974ecae6a59f
jsy4 <jsy4@users.noreply.github.com>
2021-03-08 21:05:19 -0700

    Added unity 3D module
    main navigation to the levels, moving with keyboard control example on level 3

---
+++ VirtualRetaliate/Assets/Codes/LvMgmt.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LvMgmt : MonoBehaviour
+{
+    public void GotoStart()
+    {
+        SceneManager.LoadScene("Lv1");
+    }
+    public void GotoLv1()
+    {
+        SceneManager.LoadScene("Lv1");
+    }
+    public void GotoLv2()
+    {
+        SceneManager.LoadScene("Lv2");
+    }
+    public void GotoLv3()
+    {
+        SceneManager.LoadScene("Lv3");
+    }
+    public void GotoLv4()
+    {
+        SceneManager.LoadScene("Lv4");
+    }
+    public void GoStartPage()
+    {
+        SceneManager.LoadScene("startmenu");
+    }
+
+}
