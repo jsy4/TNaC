3f499b306fbd7ecd1d98e7b44e60e7d27208ccb4
jsy4 <jsy4@users.noreply.github.com>
2021-03-25 16:55:15 -0600

    testing camera
    camera view worked. pushing back to github

---
+++ VirtualRetaliate/Assets/Codes/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = 0.125F;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothTime);
+        transform.LookAt(target);
+    }
+
+}
