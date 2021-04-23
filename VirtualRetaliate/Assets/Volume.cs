06d368924e7a75b684465dfe24ab75608e2fd811
jsy4 <jsy4@users.noreply.github.com>
2021-04-22 23:03:06 -0600

    background sound and citation page added
    background sound is added for main page and level 2 (continuous through run time) and Citations scene is added for citations.

---
+++ VirtualRetaliate/Assets/Volume.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class Volume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public void setVolume(float sliderValue) 
+    {
+        mixer.SetFloat("Volume", Mathf.Log10(sliderValue) * 20);
+    } // youtube.com/watch?v=xNHSGMKtlv4
+}
