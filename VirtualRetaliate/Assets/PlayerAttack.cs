8409add4d5cbb26c6f0f1d3f0665dedb615b4dee
Miguel Fernandez <mfrnandz@nmsu.edu>
2021-05-09 13:16:17 -0600

    Combat Implementation

---
+++ VirtualRetaliate/Assets/PlayerAttack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+
+	public Camera cam;
+	public GameObject Hand;
+	public Weapon myWeapon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myWeapon = Hand.GetComponentInChildren<Weapon>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(0)){
+        	DoAttack();
+        }
+    }
+
+    private void DoAttack(){
+    	Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+    	RaycastHit hit;
+
+    	if(Physics.Raycast(ray, out hit, myWeapon.attackRange)){
+    		if(hit.collider.tag == "Enemy"){
+    			EnemyHealth eHealth = hit.collider.GetComponent<EnemyHealth>();
+    			eHealth.TakeDamage(myWeapon.attackDamage);
+    		}
+    	}
+    }
+}
